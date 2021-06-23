aec1736dc065a9340d795ab5316c013dab7ca0cb
HawDevelopment <70876593+HawDevelopment@users.noreply.github.com>
2021-06-23 15:37:51 +0200

    Strategy

---
+++ src/Strategy.lua
@@ -0,0 +1,66 @@
+--[[
+    Strategy class
+    HawDevelopment
+    23/06/2021
+--]]
+local t = require(game.ReplicatedStorage.t)
+
+---class @Strategy
+local Strategy = {}
+Strategy.__index = Strategy
+
+--- Creates a new Strategy
+---@param handles table<string | number, fun(any): any>
+---@param start number
+function Strategy.new(handles, start)
+	assert(t.table(handles))
+	assert(#handles < 1, "Expected a table with more than zero entrys")
+
+	local self = setmetatable({}, Strategy)
+
+	self.__handles = handles
+	_, self.__strategy = next(handles)
+
+	return self
+end
+
+--- Sets the current Strategy
+---@param name string @The name of strategy to set
+---@return nil
+function Strategy:set(name)
+	assert(t.union(t.string, t.number)(name))
+
+	if not self.__handles[name] then
+		return error(("Strategy named %s could not be found"):format(tostring(name)), 2)
+	end
+
+	self.__strategy = self.__handles[name]
+end
+
+--- Calls the current set strategy, of none is found it errors
+---@vararg any
+---@return any
+function Strategy:call(...)
+	assert(self.__strategy ~= nil, "Strategy not set")
+
+	return self.__strategy(...)
+end
+
+Strategy.__index = function(self, index)
+	if type(Strategy[index]) == "function" then
+		return function(...)
+			return Strategy[index](self, ...)
+		end
+	else
+		return Strategy[index]
+	end
+end
+Strategy.__call = function(self, ...)
+	return self.call(...)
+end
+
+return setmetatable(Strategy, {
+	__call = function(self, ...)
+		return self.new(...)
+	end,
+})
---
+++ tests/server/Strategy.spec.lua
@@ -0,0 +1,119 @@
+--[[
+    Strategy tests
+    HawDevelopment
+    23/06/2021
+--]]
+
+return function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local lib = require(ReplicatedStorage.libs)
+
+	---@module Strategy
+	local Strategy = require(lib.Strategy)
+
+	local handles = {
+		Support = function(name)
+			return "Support " .. tostring(name)
+		end,
+		Ticket = function(name)
+			return "Ticket " .. tostring(name)
+		end,
+	}
+
+	describe("Strategy.new", function()
+		it("should return a valid strategy", function()
+			local strat = Strategy.new(handles, 1)
+
+			expect(strat.__handles).to.be.a("table")
+			expect(strat.__strategy).to.be.a("function")
+		end)
+
+		it("should error if given wrong arguments", function()
+			expect(function()
+				Strategy.new("Sup")
+			end).to.be.throw()
+
+			expect(function()
+				Strategy.new(69)
+			end).to.be.throw()
+		end)
+
+		it("should be able to be created using __call", function()
+			expect(function()
+				Strategy(handles)
+			end).to.never.be.throw()
+		end)
+	end)
+
+	describe("Strategy.set", function()
+		local strat
+		beforeEach(function()
+			strat = Strategy.new(handles)
+		end)
+
+		it("should set the current strategy", function()
+			-- Reset strategy
+			strat.__strategy = nil
+			strat.set("Ticket")
+
+			expect(strat.__strategy).to.be.a("function")
+		end)
+
+		it("should error if given wrong arguments", function()
+			expect(function()
+				strat.set(game)
+			end).to.be.throw()
+
+			expect(function()
+				strat.set()
+			end).to.be.throw()
+
+			expect(function()
+				strat.set({})
+			end).to.be.throw()
+		end)
+
+		it("should error if strategy isnt found", function()
+			expect(function()
+				strat.set("Developer")
+			end).to.be.throw()
+		end)
+	end)
+
+	describe("Strategy.call", function()
+		local strat
+		beforeEach(function()
+			strat = Strategy.new(handles, 1)
+		end)
+
+		it("should call strategy and return its value", function()
+			-- Just to be sure
+			strat.set("Support")
+
+			local ret
+			expect(function()
+				ret = strat.call("1")
+			end).to.never.be.throw()
+
+			expect(ret).to.be.equal("Support 1")
+		end)
+
+		it("should error if no strategy is set", function()
+			strat.__strategy = nil
+			expect(function()
+				strat.call()
+			end).to.be.throw()
+		end)
+
+		it("should be able to be called using __call", function()
+			strat.set("Ticket")
+
+			local ret
+			expect(function()
+				ret = strat("1")
+			end).to.never.be.throw()
+
+			expect(ret).to.be.equal("Ticket 1")
+		end)
+	end)
+end
